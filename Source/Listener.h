d1f237d553f8521263c5219538dafbfbc6357a95
Justin Harrison <justin@matthin.com>
2015-04-04 08:29:37 -0400

    Add doxygen documentation

--- Source/Listener.h
+++ Source/Listener.h
@@ -8,6 +8,10 @@ class Listener : public Socket {
 public:
     Listener();
 
+    /**
+     *  Blocks until a socket connection can be accepted.
+     *  @return Whether the connection was succesful or not.
+     */
     bool accept(Socket* socket) noexcept;
     bool listen(unsigned short port) noexcept;
 };
--- Source/Socket.h
+++ Source/Socket.h
@@ -11,18 +11,56 @@ public:
     Socket();
     ~Socket();
 
+    /**
+     * Sends message to the connected server.
+     */
     void send(const std::string& data) noexcept;
+    
+    /**
+     * Retreives message from the connected server.
+     * @return Retrieved message.
+     */
     std::string receive(unsigned int size) noexcept;
+    
+    /**
+     * Connects to a server.
+     * @return Whether the connection was succesful or not.
+     */
     bool connect(const std::string& host, unsigned short port) noexcept;
+    
+    /**
+     * Disconnects from the currently connected server.
+     * @return Whether the disconnection was succesfull or not.
+     * @return False if not connected to any server.
+     */
     bool disconnect() noexcept;
+    
+    /**
+     * Sets send and receive timeouts.
+     */
     void setTimeout(long seconds, long microseconds) noexcept;
+    
+    /**
+     * Sets send and receive timeouts.
+     */
     void setTimeout(long seconds) noexcept;
+    
+    /**
+     * Gets the server IP address.
+     */
     std::string getRemoteAddress() const noexcept;
+    
+    /**
+     * Gets the local port in which all communication currently runs though.
+     */
     unsigned short getLocalPort() const noexcept;
     
     int handle;
 
 protected:
+    /**
+     * Creates c-style address representation from a standard IP address and port.
+     */
     sockaddr_in
     createAddress(const std::string& host, unsigned short port) const noexcept;
 };
